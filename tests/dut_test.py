8781405d9d41026d2f95ff4b3b3b5a96e1fc924d
junglemonkey <mithumruthulani2005@gmail.com>
2025-05-08 19:38:34 +0530

    Update dut_test.py

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -4,22 +4,12 @@ from cocotb.triggers import Timer
 
 @cocotb.test()
 async def dut_test(dut):
-    dut.a.value = 0
-    dut.b.value = 0
-    await Timer(1, units='ns')
-    assert dut.y.value == 0
+    a = (0,0,1,1)
+    b = (0,1,0,1)
+    y = (0,1,1,0)
 
-    dut.a.value = 0
-    dut.b.value = 1
-    await Timer(1, units='ns')
-    assert dut.y.value == 1
-
-    dut.a.value = 1
-    dut.b.value = 0
-    await Timer(1, units='ns')
-    assert dut.y.value == 1
-
-    dut.a.value = 1
-    dut.b.value = 1
-    await Timer(1, units='ns')
-    assert dut.y.value == 0
+    for i in range(4):
+            dut.a.value = a[i]
+            dut.b.value = b[i]
+            await Timer(1,'ns')
+            assert dut.y.value == y[i], f"Error at iteration"
