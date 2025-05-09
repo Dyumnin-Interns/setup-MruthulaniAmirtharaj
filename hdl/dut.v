45c07de62049802498526d1a23d441db1875dd02
junglemonkey <mithumruthulani2005@gmail.com>
2025-05-09 06:28:43 +0530

    Update dut.v

--- hdl/dut.v
+++ hdl/dut.v
@@ -1,9 +1,11 @@
-module and_gate (
-    input wire a,
-    input wire b,
-    output wire y
+module dut(input wire a,
+	input wire b,
+	output wire y
 );
-
-assign y = a & b;
+assign y=a^b;
+initial begin
+    $dumpfile("dut.vcd"); 
+    $dumpvars(0, dut);  
+end
 
 endmodule
